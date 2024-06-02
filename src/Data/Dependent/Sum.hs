37cb28a9d853bbaa798c9476edb200b9588231c2
Alexandre Esteves <alexfmpe@proton.me>
2024-06-02 22:42:56 +0100

    wip

--- src/Data/Dependent/Sum.hs
+++ src/Data/Dependent/Sum.hs
@@ -5,12 +5,15 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE Safe #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 module Data.Dependent.Sum where
 
 import Control.Applicative
@@ -22,8 +25,12 @@ import Data.GADT.Show
 import Data.GADT.Compare
 
 import Data.Maybe (fromMaybe)
+import Data.Some (Some(Some), foldSome)
 
 import Text.Read
+--import Control.Categorical.Bifunctor (Bifunctor)
+import Data.Functor.Identity
+import qualified Data.Functor.Product as Functor
 
 -- | A basic dependent sum type where the first component is a tag
 -- that specifies the type of the second. For example, think of a GADT
@@ -147,3 +154,106 @@ compareTaggedPrec tag1 tag2 f1 f2 = case tag1 `gcompare` tag2 of
 
 compareTagged :: forall tag f a. OrdTag tag f => tag a -> tag a -> f a -> f a -> Ordering
 compareTagged k _ x0 x1 = has' @Eq @f k $ has' @Ord @f k (compare x0 x1)
+
+--someKey :: DSum k v -> Some k
+--someKey (k :=> _) = Some k
+
+--someValue :: DSum k v -> Some v
+--someValue (_ :=> v) = Some v
+
+fromKey :: (forall x. k x -> v x) -> Some k -> DSum k v
+fromKey f = foldSome $ \k -> k :=> f k
+
+fromValue :: (forall x. v x -> k x) -> Some v -> DSum k v
+fromValue f = swapDSum . fromKey f
+
+mapKey :: (forall x. k x -> k' x) -> DSum k v -> DSum k' v
+mapKey f (k :=> v) = f k :=> v
+
+mapValue :: (forall x. v x -> v' x) -> DSum k v -> DSum k v'
+mapValue f (k :=> v) = k :=> f v
+
+forKey :: Functor f => (forall x. k x -> f (k' x)) -> DSum k v -> f (DSum k' v)
+forKey f (k :=> v) = (:=> v) <$> f k
+
+forValue :: Functor f => (forall x. v x -> f (v' x)) -> DSum k v -> f (DSum k v')
+forValue f (k :=> v) = (k :=>) <$> f v
+
+dcurry :: (DSum k v -> a) -> (forall x. k x -> v x -> a)
+dsum, duncurry :: (forall x. k x -> v x -> a) -> DSum k v -> a
+dcurry f k v = f (k :=> v)
+
+duncurry f (k :=> v) = f k v
+dsum = duncurry
+
+fromPair :: (k,v) -> DSum (Const k) (Const v)
+fromPair (k,v) = Const k :=> Const v
+
+toPair :: DSum (Const k) (Const v) -> (k,v)
+toPair (Const k :=> Const v) = (k, v)
+
+swapDSum :: DSum k v -> DSum v k
+swapDSum (k :=> v) = v :=> k
+
+
+type (~>) = NT
+data NT f g where
+  NT :: (forall x. f x -> g x) -> NT f g
+
+class Product p a b | p -> a, p -> b where
+  fst :: p -> a
+  snd :: p -> b
+
+instance Product (a,b) a b where
+  fst = Prelude.fst
+  snd = Prelude.snd
+
+instance Product (Functor.Product a b x) (a x) (b x) where
+  fst (Functor.Pair a b) = a
+  snd (Functor.Pair a b) = b
+
+instance Product (DSum a b) (Some a) (Some b) where
+  fst (k :=> _) = Some k -- someKey
+  snd (_ :=> v) = Some v -- someValue
+
+{-
+class Product p f where
+  fst :: p a b -> f a
+  snd :: p a b -> f b
+
+instance Product (,) Identity where
+  fst = Identity . Prelude.fst
+  snd = Identity . Prelude.snd
+
+instance Product DSum Some where
+  fst (k :=> _) = Some k -- someKey
+  snd (_ :=> v) = Some v --someValue
+
+instance Product Functor.Product ()
+-}
+class PFunctor p s t where first  :: s a a' -> t (p a b) (p a' b)
+class QFunctor p s t where second :: s b b' -> t (p a b) (p a b')
+class Bifunctor p r s t where bimap :: r a a' -> s b b' -> t (p a b) (p a' b')
+
+--instance (forall a b. Product (p a b) a b) => PFunctor p (->) (->) where
+--  first f p =
+
+--instance Bifunctor Functor.Product (->) (->) (->) where
+instance Bifunctor (,) (->) (->) (->) where
+  bimap f g (a,b) = (f a, g b)
+
+instance Bifunctor Functor.Product (->) (->) (->) where -- (->) (->) (->) where
+
+  bimap f g (Functor.Pair a b) = Functor.Pair (f a) (g b)
+
+instance Bifunctor DSum (~>) (~>) (->) where
+  bimap (NT fk) (NT fv) (k :=> v) = fk k :=> fv v
+
+
+{-
+instance PFunctor (,) (->) (->) where first  f (a,b) = (f a, b)
+instance QFunctor (,) (->) (->) where second f (a,b) = (a, f b)
+
+instance PFunctor DSum (~>) (->) where first  (NT f) (k :=> v) = f k :=> v -- mapKey
+instance QFunctor DSum (~>) (->) where second (NT f) (k :=> v) = k :=> f v -- mapValue
+-}
